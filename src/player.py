3ca3a4f5c07880f40b1545370766cdc87024c6bc
Jessica Williams <32785700+JayellWolfe@users.noreply.github.com>
2020-07-09 14:53:18 -0400

    Update player.py

--- src/player.py
+++ src/player.py
@@ -1,2 +1,14 @@
 # Write a class to hold player information, e.g. what room they are in
 # currently.
+from room import Room
+
+class Player:
+    def __init__(self, name,room,items=[]):
+        self.name = name
+        self.room = room
+        self.items = items
+        self.score = 0
+
+
+    def __str__(self):
+        return self.items
